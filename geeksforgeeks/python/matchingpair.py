3be0b33c617ca5e2dad68ff39c4c52513a7dcddd
jdeeps <jdeepasha@gmail.com>
2021-12-07 20:36:32 -0800

    Create matchingpair.py

---
+++ geeksforgeeks/python/matchingpair.py
@@ -0,0 +1,32 @@
+# -----------------------------------------------------------------------------------------------------------------------
+# Date Dec 6,2021
+# Problem statement :
+# { Given a set of numbers from 1 to N, each number is exactly present twice so there are N pairs.
+# In the worst-case scenario, how many numbers X should be picked and removed from the set until we find a matching pair? }
+# -------------------------------------------------------------------------------------------------------------------------
+class Solution:
+    def find (self, N):
+        # create a list with the elements ranging from 1 to N
+        # example - for N=1, it will create a list {1} and then extend itself to create {1,1}
+        list = []
+        for i in range(1,N+1):
+            list.append(i)
+        list.extend(list)
+        # following lines of code will iterate 'list' and simultaneously add it to 'matching_list'.
+        # If the same element is found in the 'matching_list' then it will return the number of times it picked to get a matching pair
+        matching_list =[]
+        for i in range(0,len(list)):
+            if list[i] in matching_list:
+               return i+1
+            matching_list.append(list[i])
+
+#{
+#  Driver Code Starts
+
+if __name__ == '__main__':
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+        ob = Solution()
+        print(ob.find(N))
+# } Driver Code Ends
